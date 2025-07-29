47d50f12707dc40f7da7c9bb6305407dcb5ecdad
uift-688 <110457073+uift-688@users.noreply.github.com>
2025-07-29 16:55:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -456,25 +456,3 @@ is_test_mode = False
 def set_test_mode():
     global is_test_mode
     is_test_mode = True
-
-# 4. ドライバ準備
-with build_system(44100, is_context=True, auto_execute=True) as (driver, loop, scheduler, sound):
-    sound.add("audio.wav", "main")
-
-    # 6. タスク定義（ループを何回進めるか決める）
-    @loop.task
-    async def task():
-        print("Go")
-        audio = await scheduler.play_soon("main")
-        scheduler.set_tps(20)
-        @loop.execute_to_tick(10)
-        async def func():
-            audio.cancel()
-            print("音声キャンセル！")
-            @loop.execute_to_tick(10)
-            async def func():
-                print("ループ終了")
-                loop.stop()
-        async for i in loop:
-            print(i)
-
